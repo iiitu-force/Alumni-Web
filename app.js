9a2c3a18a43c1d70c2416b39e8f59369e3774946
ayushjainrksh <ayushjainrksh@gmail.com>
2019-03-28 04:56:13 +0530

    Login and database fetching complete

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
-    app = express()
-    PORT = 5000
-    mongoose = require('mongoose')
+    app = express(),
+    PORT = 5000,
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser')
 
 var passport = require("passport"),
     localStrategy = require("passport-local"),
@@ -9,13 +10,14 @@ var passport = require("passport"),
 
 app.set('view engine', 'ejs')
 app.use(express.static("assets"));
+app.use(bodyParser.urlencoded({extended:true}));
 mongoose.connect("mongodb://localhost/alumni_db");
 
 //User Model
 var userSchema = new mongoose.Schema({
     firstName : String,
     lastName : String,
-    email : String,
+    username : String,
     password : String,
     contact : Number,
     branch : String,
@@ -27,14 +29,96 @@ userSchema.plugin(passportLocalMongoose);
 
 var User = mongoose.model("User", userSchema);
 
+//Express-session setup
+app.use(require("express-session")({
+    secret : "I am AJ",
+    resave : false,
+    saveUninitialized : false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+})
+
 
 app.get('/', function(req, res){
     res.render('home')
 })
 
-app.get('/view', function(req, res){
-    res.render('view')
+app.get('/view/:branch/:year', isLoggedIn, function(req, res){
+    if(req.params.branch === 'cse'){
+        fullBranch = 'Computer Science'
+    }
+    else if(req.params.branch === 'ece')
+        fullBranch = 'Electronics and Communication'
+    else
+        fullBranch = 'Information Technology'
+
+    console.log(req.params.year.substring(2,4))
+	User.find({branch: fullBranch, rollNo: {$regex: new RegExp('IIITU' + req.params.year.substring(2,4) + '.*', 'i')} }, function(err, foundUser){
+        if(err)
+            console.log(err);
+        else{
+            console.log(foundUser)
+	        res.render("view", {users : foundUser});
+        }
+	});
 })
+
+// AUTH ROUTES
+
+//Register Route
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    var user = new User({firstName : req.body.firstName, lastName : req.body.lastName, username : req.body.username, contact : req.body.contact, branch : req.body.branch, rollNo : req.body.rollNo, job : req.body.job});
+    User.register(user, req.body.password, function(err, newUser){
+        if(err)
+           console.log(err);
+        else
+        {
+            passport.authenticate("local")(req, res, function(){
+                if(err)
+                    console.log(err);
+                else
+                    res.redirect("/");
+            });
+        }
+    });
+});
+
+//Login Route
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",{
+    successRedirect : "/",
+    failureRedirect : "/"
+}));
+
+//Logout Route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+})
+
+
+function isLoggedIn(req, res, next){
+	if(req.user)
+		return next();
+	res.redirect("/");
+}
+
 
 app.listen(PORT, function(err){
     if(err)
